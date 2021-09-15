041875a861e1a6f0dec5f9b716e94919127db0a0
Raul Gutierrez Niubo <raulgniubo@gmail.com>
2021-09-15 12:18:01 -0400

    Project Created

---
+++ Basic-Weather/Basic-Weather/Basic_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Basic_WeatherApp.swift
+//  Basic-Weather
+//
+//  Created by Raul Gutierrez Niubo on 9/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Basic_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
